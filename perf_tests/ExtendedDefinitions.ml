9c43313b979535164554d9518409e064f1ca6ded
rozplokhas <rozplokhas@gmail.com>
2018-04-21 18:13:55 +0300

    Take current version of search extension

---
+++ perf_tests/ExtendedDefinitions.ml
@@ -0,0 +1,31 @@
+open GT
+open MiniKanren
+
+@type tree = Leaf | Node of tree logic * tree logic with show
+
+let show_tree = show(logic) (show tree)
+
+let (!!!) = Obj.repr
+
+
+(* Relations on lists *)
+
+let rec appendo a b ab = relation "appendo" [!!! a; !!! b; !!! ab] @@
+  conde [
+    (a === !Nil) &&& (b === ab);
+    fresh (h t ab') (?& [
+      (a === h % t); 
+      (appendo t b ab');
+      (h % ab' === ab);
+    ])
+  ]
+
+let rec reverso a b = relation "reverso" [!!! a; !!! b] @@
+  conde [
+    (a === !Nil) &&& (b === !Nil);
+    fresh (h t a') (?& [
+      (a === h % t);
+      (reverso t a');
+      (appendo a' !<h b);
+    ])
+  ]
---
+++ perf_tests/tester.ml
@@ -0,0 +1,42 @@
+open MiniKanren
+
+let empty_reifier _ _ = ""
+
+let run printer n num (repr, goal) handler =
+  Printf.printf "%s, %s answer%s {\n" 
+    repr 
+    (if n = (-1) then "all" else string_of_int n) 
+    (if n <>  1  then "s" else "");  
+  let table = List.map (fun (name, ans) -> name, Stream.take ~n:n ans) (run num goal handler) in
+  let rec show = function
+  | (_, []) :: _    -> ()
+  | table ->
+      let table' = 
+	List.map 
+	  (fun (n, x::xs) -> 
+	    Printf.printf "%s=%s; " n (printer x); 
+	    (n, xs)
+	  ) 
+	  table 
+      in
+      Printf.printf "\n";
+      show table'
+  in
+  show table;
+  Printf.printf "}\n%!"
+ 
+let qh   = fun qs       -> ["q", qs]
+let qrh  = fun qs rs    -> ["q", qs; "r", rs]
+let qrsh = fun qs rs ss -> ["q", qs; "r", rs; "s", ss]
+
+let (!) = (!!)
+
+let rec rnats = function
+| 0 -> []
+| n -> n :: rnats (n - 1)
+
+let nats n = List.rev (rnats n)
+
+let show_int      = show(logic) (show int)
+let show_int_list = show(List.logic) show_int
+let show_nat_list = show(List.logic) (show Nat.logic)
