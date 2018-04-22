505162ed27771f7d103f77c65975f4ff6789b486
Dmitry Boulytchev <dboulytchev@gmail.com>
2018-04-22 23:23:00 +0300

    Timing

--- perf_tests/tester.ml
+++ perf_tests/tester.ml
@@ -1,30 +1,50 @@
 open GT
 open MiniKanren
 
-let empty_reifier _ _ = ""
+let make_timer () =
+  let open Unix in
+  let origin = (times ()).tms_utime in
+  (fun () ->
+    (times ()).tms_utime -. origin
+  )
 
+exception Timeout
+
+let empty_reifier _ _ = ""
+            
 let run printer n num (repr, goal) handler =
-  Printf.printf "%s, %s answer%s {\n" 
-    repr 
-    (if n = (-1) then "all" else string_of_int n) 
-    (if n <>  1  then "s" else "");  
-  let table = List.map (fun (name, ans) -> name, Stream.take ~n:n ans) (run num goal handler) in
-  let rec show = function
-  | (_, []) :: _    -> ()
-  | table ->
-      let table' = 
-	List.map 
-	  (fun (n, x::xs) -> 
-	    Printf.printf "%s=%s; " n (printer x); 
-	    (n, xs)
-	  ) 
-	  table 
+  let name = Filename.chop_suffix (Filename.basename (Sys.executable_name)) ".opt" in
+  let dump =
+    try
+      Printf.printf "%s, %s answer%s {\n" 
+        repr 
+        (if n = (-1) then "all" else string_of_int n) 
+        (if n <>  1  then "s" else "");
+      let get_time = make_timer () in
+      Sys.set_signal Sys.sigalrm (Sys.Signal_handle (fun _ -> raise Timeout));
+      ignore (Unix.alarm 120);    
+      let results = run num goal handler in
+      let time    = get_time () in
+      let table = List.map (fun (name, ans) -> name, Stream.take ~n:n ans) results in
+      let rec show = function
+      | (_, []) :: _    -> ()
+      | table ->
+         let table' = 
+  	   List.map 
+	     (fun (n, x::xs) -> 
+	       Printf.printf "%s=%s; " n (printer x); 
+	       (n, xs)
+	     ) 
+	     table 
+         in
+         Printf.printf "\n";
+         show table'
       in
-      Printf.printf "\n";
-      show table'
-  in
-  show table;
-  Printf.printf "}\n%!"
+      show table;
+      Printf.printf "}\n%!";
+      Printf.sprintf "\\def\\%s{%f}\n" name time
+    with Timeout -> Printf.sprintf "\\def\\%s{---}\n" name
+  in Printf.eprintf "%s" dump
  
 let qh   = fun qs       -> ["q", qs]
 let qrh  = fun qs rs    -> ["q", qs; "r", rs]
