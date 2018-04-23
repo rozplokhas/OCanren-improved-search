f6dfd4ad769c2c847112769a0641851b3bd8ea29
Dmitry Boulytchev <dboulytchev@gmail.com>
2018-04-23 18:33:59 +0300

    Sync

--- perf_tests/appendo_100_pessimistic.ml
+++ perf_tests/appendo_100_pessimistic.ml
@@ -3,4 +3,4 @@ open MiniKanren
 open Tester
 open PessimisticDefinitions
      
-let _ = run show_int_list 101 qr (REPR (fun q r -> appendo q r (inj_list (nats 100)))) qrh
+let _ = run show_int_list (-1) qr (REPR (fun q r -> appendo q r (inj_list (nats 100)))) qrh
--- perf_tests/tester.ml
+++ perf_tests/tester.ml
@@ -13,19 +13,44 @@ exception Timeout
 let empty_reifier _ _ = ""
             
 let run printer n num (repr, goal) handler =
-  let name = Filename.chop_suffix (Filename.basename (Sys.executable_name)) ".opt" in
+  let recode s =
+    let b = Buffer.create 1024 in
+    for i=0 to String.length s - 1 do
+      match s.[i] with
+      | '_'             -> Buffer.add_string b "x"
+      | ('0'..'9') as c -> Buffer.add_string b ([|"O"; "I"; "S"; "R"; "J"; "B"; "G"; "T"; "X"; "P"|].(Char.code c - Char.code '0'))
+      | c               -> Buffer.add_char b c
+    done;
+    Buffer.contents b
+  in
+  let name' = Filename.chop_suffix (Filename.basename (Sys.executable_name)) ".opt" in
+  let name  = recode name' in
   let dump =
     try
       Printf.printf "%s, %s answer%s {\n" 
         repr 
         (if n = (-1) then "all" else string_of_int n) 
         (if n <>  1  then "s" else "");
-      let get_time = make_timer () in
-      Sys.set_signal Sys.sigalrm (Sys.Signal_handle (fun _ -> raise Timeout));
-      ignore (Unix.alarm 120);    
-      let results = run num goal handler in
-      let time    = get_time () in
-      let table = List.map (fun (name, ans) -> name, Stream.take ~n:n ans) results in
+      let rec iterate n i acc =
+        Printf.printf "Iteration %d...%!" i;
+        let get_time = make_timer () in
+        if i = 1
+        then (
+          Sys.set_signal Sys.sigalrm (Sys.Signal_handle (fun _ -> raise Timeout));
+          ignore (Unix.alarm 120);
+        );
+        Printf.printf "Before run...\n%!";
+        let results  = run num goal handler in
+        Printf.printf "After run...\n%!";        
+        let table    = List.map (fun (name, ans) -> name, Stream.take ~n:n ans) results in
+        Printf.printf "After table...\n%!";        
+        if i = 1 then Sys.set_signal Sys.sigalrm Sys.Signal_ignore;
+        let time     = get_time () in
+        Printf.eprintf "%%Current time: %f\n" time;
+        Printf.printf "yep\n%!";
+        if i = n then table, (acc +. time) /. (float n) else iterate n (i+1) (acc +. time);
+      in
+      let table, time = iterate 5 1 0.0 in
       let rec show = function
       | (_, []) :: _    -> ()
       | table ->
@@ -42,8 +67,8 @@ let run printer n num (repr, goal) handler =
       in
       show table;
       Printf.printf "}\n%!";
-      Printf.sprintf "\\def\\%s{%f}\n" name time
-    with Timeout -> Printf.sprintf "\\def\\%s{---}\n" name
+      Printf.sprintf "\\def\\%s{%f}%%%s\n" name time name'
+    with Timeout -> Printf.printf "timeout!\n%!"; Printf.sprintf "\\def\\%s{---}%%%s\n" name name'
   in Printf.eprintf "%s" dump
  
 let qh   = fun qs       -> ["q", qs]
